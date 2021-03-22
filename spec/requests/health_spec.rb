16392a54bd5e97f8c8a17be0640f43b5dc5628b0
Janchorizo <jancho@usal.es>
2021-03-22 09:39:31 +0100

    Add controller and requests tests for health endpoint.

---
+++ spec/requests/health_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Health API', type: :request do
+  let(:name) { 'healthy' }
+  describe 'GET /health' do
+    before { get '/health' }
+
+    it 'returns health' do
+      expect(json).not_to be_empty
+    end
+    it 'returns status code 200' do
+      expect(json).to have_http_status(200)
+    end
+    it 'has {healthy: bool} shape' do
+      expect(json.length).to eq(1)
+      expect(json.keys).to contain_exactly('healthy')
+    end
+    it 'is healthy' do
+      expect(json['healthy']).to be true
+    end
+  end
+end
+
