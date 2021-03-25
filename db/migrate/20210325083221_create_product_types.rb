f616708f92714dab5810462b8f588cd011ff4b74
Janchorizo <jancho@usal.es>
2021-03-25 09:33:48 +0100

    Create ProductType model.

---
+++ db/migrate/20210325083221_create_product_types.rb
@@ -0,0 +1,11 @@
+class CreateProductTypes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :product_types do |t|
+      t.string :name
+      t.string :description
+      t.bool :hidden
+
+      t.timestamps
+    end
+  end
+end
