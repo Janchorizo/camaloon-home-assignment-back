b30b441621eb2a57e36182255b4a77d0a1027591
Janchorizo <jancho@usal.es>
2021-03-25 12:47:16 +0100

    Implement field validation for product type model.

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,2 +1,10 @@
 class ProductType < ApplicationRecord
+    validates :name, :description, presence: true, length: { minimum: 1 }
+    validates :hidden, presence: true, inclusion: [true, false]
+    after_initialize :set_defaults, unless: :persisted?
+    # The set_defaults will only work if the object is new
+
+    def set_defaults
+        self.hidden = true if self.hidden.nil?
+    end
 end
