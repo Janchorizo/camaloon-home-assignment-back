bf7d4c231e9fb7bb38dafedee0fef66977ba2f4f
Janchorizo <jancho@usal.es>
2021-03-26 21:40:10 +0100

    Add specs for manufacturer model.

--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -4,7 +4,37 @@ RSpec.describe Manufacturer, type: :model do
   subject { create :manufacturer }
   let(:name) { "simple name" }
 
+  # instance integrity tests
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_presence_of(:cif) }
+  it { should validate_uniqueness_of(:cif) }
+  it { should validate_presence_of(:phone) }
+  it { should validate_presence_of(:email) }
+
   it "is valid with the factory parameters" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid with a repeated name" do
+    manufacturer = create(manufacturer, name: :name)
+    subject.name = :name
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a repeated cif" do
+    manufacturer = create(manufacturer, cif: :cif)
+    subject.cif = :cif
+    expect(subject).to_not be_valid
+  end
+
+  it "is valid without a valid website" do
+    subject.website = nil
+    expect(subject).to be_valid
+  end
+
+  it "is valid without a valid billing address" do
+    subject.billing_address = nil
     expect(subject).to be_valid
   end
 end
