e13ec72401d2e495bc1c1dfa5192bba71880a7fb
Janchorizo <jancho@usal.es>
2021-03-26 21:51:16 +0100

    Refactor product choice line specs.

--- spec/models/product_choice_line_spec.rb
+++ spec/models/product_choice_line_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe ProductChoiceLine, type: :model do
   subject { create :product_choice_line }
   let(:name) { "simple name" }
 
-  it { should validate_presence_of(:customization_choice) }
-  it { should validate_presence_of(:product) }
+  # association tests
   it { should belong_to(:customization_choice) }
   it { should belong_to(:product) }
+
+  # instance integrity tests
+  it { should validate_presence_of(:customization_choice) }
+  it { should validate_presence_of(:product) }
 
   it "is valid with the factory parameters" do
     expect(subject).to be_valid
