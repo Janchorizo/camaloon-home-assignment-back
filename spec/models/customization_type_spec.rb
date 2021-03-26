30ef69ab15586e1db66ef12ef56067830349d7be
Janchorizo <jancho@usal.es>
2021-03-26 21:08:48 +0100

    Add customization type specs.

--- spec/models/customization_type_spec.rb
+++ spec/models/customization_type_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe CustomizationType, type: :model do
   it "is valid with the factory parameters" do
     expect(subject).to be_valid
   end
+
+  it "is not valid without a valid name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a valid description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a product type" do
+    subject.product_type = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a valid product type" do
+    p_type = create :product_type
+    subject.product_type_id = p_type.id + 5.0 # a non-existing id
+    expect(subject).to_not be_valid
+  end
 end
