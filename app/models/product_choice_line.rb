602acef582cafacd6928b6e418a3495f0343add7
Janchorizo <jancho@usal.es>
2021-03-27 12:13:08 +0100

    Implement model validations for product choice line.

--- app/models/product_choice_line.rb
+++ app/models/product_choice_line.rb
@@ -1,4 +1,7 @@
 class ProductChoiceLine < ApplicationRecord
   belongs_to :customization_choice
   belongs_to :product
+
+  validates_presence_of :customization_choice
+  validates_presence_of :product
 end
