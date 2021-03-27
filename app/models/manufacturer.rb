edfc28612412f75c7e9775b9eb7566c814553b8f
Janchorizo <jancho@usal.es>
2021-03-27 12:12:04 +0100

    Implement model validation for manufacturer.

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,3 +1,14 @@
 class Manufacturer < ApplicationRecord
     has_many :customization_choice, dependent: :destroy
+
+    validates_presence_of :name
+    validates_presence_of :cif
+    validates_presence_of :phone
+    validates_presence_of :email
+
+    validates_length_of :name, :minimum => 1
+    validates_length_of :cif, :minimum => 1
+
+    validates_uniqueness_of :name
+    validates_uniqueness_of :cif
 end
