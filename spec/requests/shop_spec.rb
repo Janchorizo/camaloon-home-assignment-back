41a4e34ae7f22976338ebc83617daf9b23ca008a
Janchorizo <jancho@usal.es>
2021-03-28 22:30:02 +0200

    Add specs for the product description endpoint.

--- spec/requests/shop_spec.rb
+++ spec/requests/shop_spec.rb
@@ -110,6 +110,93 @@ RSpec.describe 'Shop API', type: :request do
     end
   end
 
+  describe 'GET /shop/products/<product_id>/specs' do
+    before { get "/shop/products/#{products.first.id}/specs" }
+
+    it 'returns the products' do
+      expect(json).not_to be_empty
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+    it 'has {status, status_code, product_id, product} shape' do
+      expect(json.length).to eq(4)
+      expect(json.keys).to contain_exactly('status', 'status_code', 'product_id', 'product')
+    end
+    it 'has a product value with {name, description, base_price, customization_options} shape' do
+      expect(json['product'].length).to eq(4)
+      expect(json['product'].keys).to contain_exactly('name', 'description', 'base_price', 'customization_options')
+      for product in json['products'] do
+          expect(product.length).to eq(1)
+          expect(product.keys).to contain_exactly('id')
+      end
+    end
+    it 'the customization options have a {name, description} shape' do
+      for c_option in json['product']['customization_options'] do
+        expect(c_option.length).to eq(2)
+        expect(c_option.keys).to contain_exactly('name', 'description')
+      end
+    end
+  end
+
+  describe 'GET /shop/products/<product_id>/specs returns empty customization options if there aren\'t' do
+    before { get "/shop/products/#{products[1].id}/specs" }
+
+    it 'returns the products' do
+      expect(json).not_to be_empty
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+    it 'the customization options array has zero length' do
+      expect(json['product']['customization_options'].length).to eq(0)
+    end
+  end
+
+  describe 'GET /shop/products/<product_id>/specs from a hidden category' do
+    before { get "/shop/products/#{products_in_hidden_category.first.id}/specs" }
+
+    it 'returns a response' do
+      expect(json).not_to be_empty
+    end
+    it 'returns status code 403' do
+      expect(response).to have_http_status(403)
+    end
+    it 'has {status, status_code} shape' do
+      expect(json.length).to eq(2)
+      expect(json.keys).to contain_exactly('status', 'status_code')
+    end
+  end
+
+  describe 'GET /shop/products/<product_id>/specs for a hidden product' do
+    before { get "/shop/products/#{hidden_products.first.id}/specs" }
+
+    it 'returns a response' do
+      expect(json).not_to be_empty
+    end
+    it 'returns status code 403' do
+      expect(response).to have_http_status(403)
+    end
+    it 'has {status, status_code} shape' do
+      expect(json.length).to eq(2)
+      expect(json.keys).to contain_exactly('status', 'status_code')
+    end
+  end
+
+  describe 'GET /shop/products/<product_id>/specs for non-existing product' do
+    before { get "/shop/products/#{products.last.id + 10}/specs" }
+
+    it 'returns a response' do
+      expect(json).not_to be_empty
+    end
+    it 'returns status code 404' do
+      expect(response).to have_http_status(404)
+    end
+    it 'has {status, status_code} shape' do
+      expect(json.length).to eq(2)
+      expect(json.keys).to contain_exactly('status', 'status_code')
+    end
+  end
 #  describe 'GET /shop/product/<product_id>/specs' do
 #    before { get '/shop/product/<product_id>/specs' }
 #
