f75eb0dfe4811625f364b7da6a3e98358aeb5ad6
Janchorizo <jancho@usal.es>
2021-03-29 00:03:18 +0200

    Check for missing attributes before setting defaults in case a projection was made.

--- app/models/customization_choice.rb
+++ app/models/customization_choice.rb
@@ -7,14 +7,18 @@ class CustomizationChoice < ApplicationRecord
   validates_presence_of :model_ref
   validates_presence_of :manufacturer
   validates_presence_of :customization_type
-  validates :extra_cost, :numericality => true
+  validates :extra_cost, :presence => true, :numericality => true
   validates :stock, :presence => true, :numericality => {:greater_than => -1}
 
   after_initialize :set_defaults
   # The set_defaults will only work if the object is new
 
   def set_defaults
-    self.extra_cost ||= 0.0
-    self.stock ||= 0.0
+    if self.has_attribute?(:extra_cost)
+      self.extra_cost ||= 0.0
+    end
+    if self.has_attribute?(:stock)
+      self.stock ||= 0.0
+    end
   end
 end
