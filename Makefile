3e3ac853025f4d0fa33198290e67a88c6fe46911
Janchorizo <jancho@usal.es>
2021-03-28 12:06:38 +0200

    Update makefile to save and load the default environment from a file.

--- Makefile
+++ Makefile
@@ -1,20 +1,62 @@
+#!make
+-include envfile
+export
+
 DB_HOST="localhost"
 DB_USER="local_user"
 DB_PASSWORD="local_user"
 DB_NAME="camaloon_local"
-RAILS_ENV="default"
+RAILS_ENV?=""
+TEST_ENV?="test"
 
 configure:
 	bash ./config.sh $(RAILS_ENV)
 
-test:
-	DB_URL="postgres://$(DB_USER):$(DB_PASSWORD)@$(DB_HOST)/$(DB_NAME)" \
-		RAILS_ENV=$(RAILS_ENV) bundle exec rspec
+env:
+ifneq ($(RAILS_ENV),'')
+	@echo "$(RAILS_ENV)\n"
+else
+	@echo "The environment is not set.\nCall make configure -e RAILS_ENV=<env>"
+	@echo "\nThe database config file and enfile will be generated. The specified"
+	@echo "environment will be used unless tests are run, in which case the test"
+	@echo "rails environment will be used.\n"
+endif
 
-serve:
-	RAILS_ENV=$(RAILS_ENV) rails s
+env_is_present:
+ifeq ($(RAILS_ENV),'')
+	@echo "The environment is not set.\nCall make configure -e RAILS_ENV=<env>"
+	@echo "\nThe database config file and enfile will be generated. The specified"
+	@echo "environment will be used unless tests are run, in which case the test"
+	@echo "rails environment will be used.\n"
+	exit 1
+endif
+
+test:
+	#DB_URL="postgres://$(DB_USER):$(DB_PASSWORD)@$(DB_HOST)/$(DB_NAME)"
+	RAILS_ENV=$(TEST_ENV) bundle exec rspec
+
+serve: env_is_present
+	RAILS_ENV=$(RAILS_ENV) rails server -e $(RAILS_ENV)
+
+dbconsole: env_is_present
+	RAILS_ENV=$(RAILS_ENV) rails dbconsole
+
+db_drop: env_is_present
+	RAILS_ENV=$(RAILS_ENV) rails db:drop
+
+db_create: env_is_present
+	RAILS_ENV=$(RAILS_ENV) rails db:create
+
+db_migrate: env_is_present
+	RAILS_ENV=$(RAILS_ENV) rails db:migrate
+
+db_recreate: env_is_present db_drop db_create
+	RAILS_ENV=$(RAILS_ENV) rails db:schema:load
+
+db_seed: env_is_present
+	RAILS_ENV=$(RAILS_ENV) rails db:seed
 
 install:
 	RAILS_ENV=$(RAILS_ENV) bundle install
 
-$PHONY: test serve install configure
+$PHONY: configure env env_is_present test serve install dbconsole db_drop db_create db_migrate db_migrate db_recreate db_seed
--- config.sh
+++ config.sh
@@ -15,41 +15,57 @@
 # https://github.com/rails/rails/issues/32926
 # https://github.com/rails/rails/issues/28827
 
-env=$1
+RAILS_env=$1
 echo "Using $1 environment"
 
 case $env in
   "default")
+    echo
     echo "⮩ Setting config/database.yml to use default settings . . ."
+    echo '  echo "RAILS_ENV=default" > envfile'
+    echo "RAILS_ENV=default" > envfile
     echo "  cp ./config/database_base.yml ./config/database.yml"
     cp ./config/database_base.yml ./config/database.yml
     echo "⮪ Finished"
+    echo
     ;;
   "development")
+    echo
     echo "⮩ Setting config/database.yml to use the merged development and default settings . . ."
+    echo '  echo "RAILS_ENV=development" > envfile'
+    echo "RAILS_ENV=development" > envfile
     echo "  cp ./config/database_dev.yml ./config/database.yml"
     cp ./config/database_base.yml ./config/database.yml
     echo "  cat ./config/database_dev.yml >> ./config/database.yml"
     cat ./config/database_dev.yml >> ./config/database.yml
     echo "⮪ Finished"
+    echo
     ;;
   "test")
+    echo
     echo "⮩ Setting config/database.yml to use the merged development and default settings . . ."
+    echo '  echo "RAILS_ENV=test" > envfile'
+    echo "RAILS_ENV=test" > envfile
     echo "  cp ./config/database_base.yml ./config/database.yml"
     cp ./config/database_base.yml ./config/database.yml
     echo "  cat ./config/database_test.yml >> ./config/database.yml"
     cat ./config/database_test.yml >> ./config/database.yml
     echo "⮪ Finished"
+    echo
   ;;
   "production")
+    echo
     echo "⮩ Setting config/database.yml to use the merged development and default settings . . ."
+    echo '  echo "RAILS_ENV=production" > envfile'
+    echo "RAILS_ENV=production" > envfile
     echo "  cp ./config/database_base.yml ./config/database.yml"
     cp ./config/database_base.yml ./config/database.yml
     echo "  cat ./config/database_prod.yml >> ./config/database.yml"
     cat ./config/database_prod.yml >> ./config/database.yml
     echo "⮪ Finished"
+    echo
   ;;
   *)
-    echo "RAILS_ENV must be one of [default,development,test,production]."
+    echo "RAILS_ENV must be one of [default,development,test,production].\n"
   ;;
 esac
