d6fad1a0cafcc9d41c424179b611ad875cea65bd
Janchorizo <jancho@usal.es>
2021-03-28 17:44:25 +0200

    Implement /shop/categories endpoint.

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,2 +1,15 @@
 class ShopController < ApplicationController
+    def categories
+        category_entries = ProductType.where(hidden: false)
+        categories = category_entries.map {
+            |entry| {:name => entry.name, :id => entry.id}
+        }
+
+        response = {
+            :status => 'OK',
+            :status_code => 200,
+            :categories => categories
+        }
+        json_response(response)
+    end
 end
