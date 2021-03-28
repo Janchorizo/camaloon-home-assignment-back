78c0a2bcf34c7d90872ac6cc20cefcd488307e50
Janchorizo <jancho@usal.es>
2021-03-28 17:23:07 +0200

    Change unique fields to use a sequence.

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
+    sequence :productName do |n|
+        "product_#{n}"
+    end
+
     factory :product do
-        name { Faker::Lorem.word }
+        name { generate(:productName) }
         description { Faker::Coffee.notes }
         base_price { Faker::Number.between(from: 1, to: 1000) }
         hidden { true }
