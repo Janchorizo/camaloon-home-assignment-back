024cb4e9f89f07d67d91688be1edd9070b7b5d75
Janchorizo <jancho@usal.es>
2021-03-29 00:02:29 +0200

    Add sequence for customization type factory.

--- spec/factories/customization_types.rb
+++ spec/factories/customization_types.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
+    sequence :customizationName do |n|
+        "customization_#{n}"
+    end
+
     factory :customization_type do
-        name { Faker::Lorem.word }
+        name { generate(:customizationName) }
         description { Faker::Coffee.notes }
         product_type
     end
